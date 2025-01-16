7897a01a9bc8c061eb508f28eec7ccb2fd036c80
nihaalxz <nihaal1010@gmail.com>
2025-01-16 20:22:29 +0530

    major changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,32 @@
 $(document).ready(() => {
-    $(".task-list").hide();
-    $("#add-btn").click(() => {
-        let task = $("#task-bar").val();
-        let desc = $("#task-desc").val();
-        if(task===""||desc==="")
-        {
-            alert("Please enter details")
-            return;
-        }
-        
-        $("#task-id").text(task);
-        $("#desc-id").text(desc);
-        $(".task-list").show();
-        
-    })
-    
-    $("#btn-delete").click(() => {
-        $(".task-list").remove();
+  $("#add-btn").click(() => {
+    let task = $("#task-bar").val();
+    let desc = $("#task-desc").val();
+    if (task === "" || desc === "") {
+      alert("Please enter task and description.");
+      return;
+    }
+    $("#task-bar").val("");
+    $("#task-desc").val("");
+    let taskbar = `<div class="taskbox">
+        <div>
+        <p class="task-name">${task}</p>
+        <p class="task-desc">${desc}</p>
+        </div>
+        <div>
+        <button id=btn-delete>❌</button>
+        <button id=btn-done>✅</button>
+        </div>
+        </div>`;
+    $(".task-items").append(taskbar);
+
+    $(".task-items").on("click", "#btn-delete", function () {
+      $(this).closest(".taskbox").remove();
     });
 
-    $("#btn-done").click(() => {
-        $(".task-list").remove();
-        alert("Task done!");
-    })
-
+    $(".task-items").on("click", "#btn-done", function () {
+      $(this).closest(".taskbox").remove();
+      alert("Task is done");
+    });
+  });
 });
